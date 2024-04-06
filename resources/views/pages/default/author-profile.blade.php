9beacb91161d728e8679a7352477b948e228ddab
Segrid Renne-George <segrid2011@hotmail.com>
2024-04-06 18:44:44 -0400

    author profile

--- resources/views/pages/default/author-profile.blade.php
+++ resources/views/pages/default/author-profile.blade.php
@@ -1,1 +1,33 @@
 <x-mylayouts.layout-default>
+
+    @include('pages.default.author-profile-area')
+
+    @include('pages.default.author-portfolio-area')
+
+
+
+
+
+    <!-- Nav tabs -->
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="tab" href="#home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#menu1">Menu 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div class="tab-pane container active" id="home">...</div>
+    <div class="tab-pane container fade" id="menu1">...</div>
+    <div class="tab-pane container fade" id="menu2">...</div>
+  </div>
+
+
+
+</x-mylayouts.layout-default>
--- resources/views/pages/default/author-writing.blade.php
+++ resources/views/pages/default/author-writing.blade.php
@@ -1,3 +1,56 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+<x-mylayouts.layout-default>
+
+    <div class="card">
+        <div class="card-body">Basic card</div>
+    </div>
+
+    <a class="btn btn-success" href="{{ route('authors.show',['author'=>$author->id]) }}">Return</a>
+
+    <h1>{$writing->title}</h1>
+    <p>Written By: <a href="http://localhost:8000/authors/1">{{ $author->name }}</a></p>
+    <p>Read Time: {{ $writing->read_time . ' ' . $writing->read_unit }}</p>
+    <p>Category: {{ $writing->type }}</p>
+    <hr>
+
+    <div class="content">
+        <div class="writing-image text-center">
+            <img style="max-height: 200px; text-align: center;" src="{{ $writing->getImage() }}" alt="Image">
+        </div>
+
+        {{ $writing->description }}
+    </div>
+    </div>
+
+
+
+
+
+</x-mylayouts.layout-default>
+
+
+
+<div class="card">
+</div class="card-body">
+
+<h1>Test story</h1>
+<p>Written By: <a href="http://localhost:8000/authors/1">{{ $author->name }}</a></p>
+<p>Read Time: {{ $writing->read_time . ' ' . $writing->read_unit }}</p>
+<p>Category: {{ $writing->type }}</p>
+<hr>
+
+<div class="content">
+    <div class="writing-image text-center">
+        <img style="max-height: 200px; text-align: center;"
+            src="http://localhost:8000/storage/images/profile/Qc1K7UZLDYCx0GxeLiodBQaiBXsYW1z1yAET5jUN.webp"
+            alt="Image">
+    </div>
+
+    {{ $writing->description }}
 </div>
+</div>
+
+
+
+
+
+</x-mylayouts.layout-default>
