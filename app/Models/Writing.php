f02b94a42935d43aa27bd72af5f2a13072ec934e
Segrid Renne-George <segrid2011@hotmail.com>
2024-04-15 21:46:29 -0400

    writing

--- app/Models/Writing.php
+++ app/Models/Writing.php
@@ -9,11 +9,17 @@ class Writing extends Model
 {
     use HasFactory;
 
-    public function getLink($user_id, $writing_id)
+    public function  getLink($user_id, $writing_id)
     {
         return route('authors.writing', ['author' => $user_id, 'id' => $writing_id]);
     }
 
+    /**
+     *Get a specific image for a writing or article 
+     *Example , when a user views a writing , this code will show
+     * the image for a writing
+     *
+     */
     public function getImage()
     {
         return asset('storage/'. $this->image);
