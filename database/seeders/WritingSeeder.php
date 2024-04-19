63b14c3d3169a496a1453713439f6d012ed53288
Segrid Renne-George <segrid2011@hotmail.com>
2024-04-18 22:04:00 -0400

    writing seeder

--- database/seeders/WritingSeeder.php
+++ database/seeders/WritingSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class WritingSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class WritingSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        DB::table('writings')->insert([
+            [
+                'user_id' => 1,
+                'type' => 'poem',
+                'title' => 'Programming can be frustrating',
+                'description' => 'hello world, goodbye world, oh I\'m confused because programming hurts',
+                'image' => '/images/media/default.jpg',
+                'read_time' => 1,
+                'read_unit' => 'minute',
+                'created_at' => Carbon::now(),
+                'updated_at' => null,
+            ],
+        ]);
+
+
     }
 }
