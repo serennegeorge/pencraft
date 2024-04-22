5c47c2b41641c8ffbc95da23659e768b5ada65ad
Segrid Renne-George <segrid2011@hotmail.com>
2024-04-22 18:30:18 -0400

    Writing Test

--- tests/Unit/WritingTest.php
+++ tests/Unit/WritingTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Writing;
+use Tests\TestCase;
 
 class WritingTest extends TestCase
 {
@@ -15,6 +16,34 @@ class WritingTest extends TestCase
     }
 
     /**
+     * Test and unsure that the Get Link returns to the correct Link
      *
+     * @return void
      */
-}
+    public function testGetLink()
+    {
+        // Goal: test getLink returns the correct link
+
+        // Action
+        $writing = new Writing();
+
+        // Compare the results
+        $this->assertEquals('http://localhost/authors/1/writing/1', $writing->getLink(1, 1));
+    }
+
+    /**
+    * Undocumented function.
+     *
+     * @return void
+     */
+    public function testGetImage()
+    {
+        // Goal: test to ensure getImage returns the correct link for the image
+        // Action: create a writing to get the getImage
+        $writing = new Writing();
+        $writing->image = 'images/media/default.jpg';
+        // Compare the results
+        $this->assertEquals('http://localhost/storage/images/media/default.jpg', $writing->getImage());
+    }
+
+    }
